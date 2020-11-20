96c8c8f236298cc0ef08639be02a764b8e5ba560
Ubuntu <ubuntu@ip-172-31-91-206.ec2.internal>
2020-11-20 19:27:30 +0000

    intial messege

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by Donggeun Lim
+# Created on November 2020
+# This is the "Hello, World" program, with proper style
+
+
+def main():
+    print("Hello, World")
+
+
+if __name__ == "__main__":
+    main()
